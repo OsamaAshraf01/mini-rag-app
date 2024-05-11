ba9ddb1701fe8c25b992b6d25ec400908491cd42
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-05-11 11:50:56 +0300

    Creat config.py file in helpers

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI, APIRouter
-import os
+from fastapi import FastAPI, APIRouter, Depends
+from helpers.config import get_settings, Settings
 
 base_router = APIRouter(
     prefix='/api/v1'
 )
 
 @base_router.get('/')
-async def welcome():
-    app_name = os.getenv('APP_NAME')
-    app_version = os.getenv('APP_VERSION')
+async def welcome(app_settings:Settings = Depends(get_settings)):  # Make the variable as a Depend to ensure that it is available before starting function's work
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
     return {
         "app_name" : app_name,
         "app_verion" : app_version,   
