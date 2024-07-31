9f4cc82ca255d603969756a0fe687745e0a826eb
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-07-31 18:29:12 +0300

    Depends function importance

--- src/routes/base.py
+++ src/routes/base.py
@@ -5,6 +5,9 @@ base_router = APIRouter(
     prefix='/api/v1'
 )
 
+# Depends function is very impoortant. It makes your application more efficient
+# It means that: Don't start in the logic of the functino until you get a ressponse from
+# a specific function
 @base_router.get('/')
 async def welcome(app_settings:Settings = Depends(get_settings)):  # Make the variable as a Depend to ensure that it is available before starting function's work
     app_name = app_settings.APP_NAME
