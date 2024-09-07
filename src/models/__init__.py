bd87b89647653bc91d747364a4d2ca927f93a72e
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-09-07 12:56:18 +0300

    Modify __init__ files

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,5 @@ from .enums.ProcessingEnum import ProcessingEnum
 from .db_schemes.data_chunck import DataChunck
 from .ProjectModel import ProjectModel
 from .ChunckModel import ChunckModel
+from .BaseDataModel import BaseDataModel
+from .AssetsModel import AssetsModel
--- src/models/db_schemes/__init__.py
+++ src/models/db_schemes/__init__.py
@@ -1,3 +1,4 @@
 from .project import Project
 from .data_chunck import DataChunck
+from .assets import Asset
  
