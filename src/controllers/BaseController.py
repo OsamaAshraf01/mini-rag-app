9b109a830b6591ee5b6bcaf24cd85982faab759d
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-09-13 00:14:20 +0300

    Add get_database_path to base controller and create database folder in assets and ignoring it.

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -8,6 +8,20 @@ class BaseController:
         self.base_dir = os.path.dirname( os.path.dirname(__file__) )  # To get the dir of src folder
         self.files_dir = os.path.join(self.base_dir, "assets/files")
 
+        self.database_dir = os.path.join(self.base_dir, "assets/database")
+
 
     def generate_random_string(self, length:int = 12):
         return ''.join(random.choices(string.ascii_lowercase + string.digits,k=length))
+    
+
+    def get_database_path(self, db_folder):
+        db_path = os.path.join(
+            self.database_dir,
+            db_folder
+        )
+
+        if not os.path.exists(db_path):
+            os.makedirs(db_path)
+
+        return db_path
